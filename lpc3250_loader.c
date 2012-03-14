a3234ce7fd6149505b0063727db4420456759ceb
Sergey Yakovlev <yakor_spb@mail.ru>
2012-03-15 01:19:40 +0400

    initial project version

---
+++ lpc3250_loader.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+
+int main  (int argc, char *argv[], char *env[] )
+{
+  printf ("lpc3250_loader\n");
+
+  return 0;
+}
