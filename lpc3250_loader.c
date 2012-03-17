82203bd16e9f11119d9b8aea8e576286827ed507
Sergey Yakovlev <yakor_spb@mail.ru>
2012-03-17 15:49:54 +0400

    indent: .c indented

--- lpc3250_loader.c
+++ lpc3250_loader.c
@@ -21,54 +21,59 @@
 #include "read_config.h"
 #include "lpc3250_loader.h"
 
-int main  (int argc, char *argv[], char *env[] )
+int
+main (int argc, char *argv[], char *env[])
 {
-  int i;
-  config_t config;
-  char conf_file_n[200];
-  wordexp_t we;
-  int port_fd;
+  int             i;
+  config_t        config;
+  char            conf_file_n[200];
+  wordexp_t       we;
+  int             port_fd;
 
 
-  sprintf(conf_file_n,CONFIG_FILE_NAME);
-  wordexp(conf_file_n, &we, 0);
-  strcpy(conf_file_n,we.we_wordv[0]);
-  wordfree(&we);
+  sprintf (conf_file_n, CONFIG_FILE_NAME);
+  wordexp (conf_file_n, &we, 0);
+  strcpy (conf_file_n, we.we_wordv[0]);
+  wordfree (&we);
 
   printf ("lpc3250_loader\n");
-  printf("Use config: %s\n",conf_file_n);
+  printf ("Use config: %s\n", conf_file_n);
 
-  read_config_yaml(conf_file_n,&config);
+  read_config_yaml (conf_file_n, &config);
 
-  printf("Port: %s\n",config.port);
-  for(i=0;i<config.qty_exec;i++)
+  printf ("Port: %s\n", config.port);
+  for (i = 0; i < config.qty_exec; i++)
     {
-      printf("i: %i\n",i);
-      printf("PrimaryFileName: %s\n",config.executables[i].primary_filename);
-      printf("IRAMaddress: %x\n",config.executables[i].iram_address);
-      printf("SecondaryFileName: %s\n",config.executables[i].secondary_filename);
-      printf("SDRAMaddress: %x\n",config.executables[i].sdram_address);
+      printf ("i: %i\n", i);
+      printf ("PrimaryFileName: %s\n", config.executables[i].primary_filename);
+      printf ("IRAMaddress: %x\n", config.executables[i].iram_address);
+      printf ("SecondaryFileName: %s\n", config.executables[i].secondary_filename);
+      printf ("SDRAMaddress: %x\n", config.executables[i].sdram_address);
     }
 
   port_fd = open (config.port, O_RDWR | O_NOCTTY | O_NDELAY);
 
-  setup_port(port_fd);
-  if(wait_byte(port_fd,'5',1)) printf("'5'\n");
-  send_byte(port_fd,'A');
-  if(wait_byte(port_fd,'5',0)) printf("'5'\n");
-  send_byte(port_fd,'U');
-  send_byte(port_fd,'3');
-  if(wait_byte(port_fd,'R',0)) printf("'R'\n");
+  setup_port (port_fd);
+  if (wait_byte (port_fd, '5', 1))
+    printf ("'5'\n");
+  send_byte (port_fd, 'A');
+  if (wait_byte (port_fd, '5', 0))
+    printf ("'5'\n");
+  send_byte (port_fd, 'U');
+  send_byte (port_fd, '3');
+  if (wait_byte (port_fd, 'R', 0))
+    printf ("'R'\n");
 
   return 0;
 }
 
-int setup_port(int port_fd)
+int
+setup_port (int port_fd)
 {
-  struct termios tio;
-  tcgetattr(port_fd,&tio);
-  cfsetispeed(&tio, B115200);
-  cfsetospeed(&tio, B115200);
+  struct termios  tio;
+  tcgetattr (port_fd, &tio);
+  cfsetispeed (&tio, B115200);
+  cfsetospeed (&tio, B115200);
   tio.c_cflag &= ~PARENB;
   tio.c_cflag &= ~CSTOPB;
   tio.c_cflag &= ~CSIZE;
@@ -77,65 +82,69 @@ int setup_port(int port_fd)
   tio.c_iflag = IGNBRK;
   tio.c_oflag = 0;
   tio.c_lflag = 0;
-  tio.c_cc[VTIME]=5;
+  tio.c_cc[VTIME] = 5;
 
-  tcflush(port_fd, TCOFLUSH);
-  tcflush(port_fd, TCIFLUSH);
-  tcsetattr(port_fd,TCIFLUSH,&tio);
+  tcflush (port_fd, TCOFLUSH);
+  tcflush (port_fd, TCIFLUSH);
+  tcsetattr (port_fd, TCIFLUSH, &tio);
   return 1;
 }
 
-int wait_byte (int port_fd,char byte,int skip)
+int
+wait_byte (int port_fd, char byte, int skip)
 {
-  struct pollfd poller;
-  int bytes;
-  char answer[2];
-  int ok;
-  int i;
-  int wait_time;
+  struct pollfd   poller;
+  int             bytes;
+  char            answer[2];
+  int             ok;
+  int             i;
+  int             wait_time;
 
-  wait_time=100;
-  if(skip) wait_time=10000;
+  wait_time = 100;
+  if (skip)
+    wait_time = 10000;
 
-  ok=0;
-  answer[1]=0;
-  printf("Waiting...\n");
-  poller.fd=port_fd;
-  poller.events=POLLIN;
-  poller.revents=0;
-  ok=poll(&poller,1,wait_time);
+  ok = 0;
+  answer[1] = 0;
+  printf ("Waiting...\n");
+  poller.fd = port_fd;
+  poller.events = POLLIN;
+  poller.revents = 0;
+  ok = poll (&poller, 1, wait_time);
 
-  if(ok==0) return 0;
+  if (ok == 0)
+    return 0;
 
   ioctl (port_fd, FIONREAD, &bytes);
 
-  for (i=0;i<bytes;i++)
+  for (i = 0; i < bytes; i++)
     {
-      read(port_fd,answer,1);
+      read (port_fd, answer, 1);
     }
 
-  if ((answer[0]==byte) && skip)
+  if ((answer[0] == byte) && skip)
     {
-      usleep(500000);
+      usleep (500000);
       ioctl (port_fd, FIONREAD, &bytes);
-      for (i=0;i<bytes;i++)
-        {
-          read(port_fd,answer,1);
-        }
+      for (i = 0; i < bytes; i++)
+	{
+	  read (port_fd, answer, 1);
+	}
     }
 
-  if(answer[0]!=byte)
+  if (answer[0] != byte)
     {
-      printf("bad answer\n");
+      printf ("bad answer\n");
       return 0;
     }
-   printf("good answer\n");
+  printf ("good answer\n");
   return 1;
 }
 
-int send_byte (int port_fd,char byte)
+int
+send_byte (int port_fd, char byte)
 {
-  int ok;
-  ok=write (port_fd,&byte,1);
+  int             ok;
+  ok = write (port_fd, &byte, 1);
   return 1;
 }
