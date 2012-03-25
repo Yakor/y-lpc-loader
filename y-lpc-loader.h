b2c70df41428f7b019831ad812ef9dc3f0139509
Sergey Yakovlev <yakor_spb@mail.ru>
2012-03-26 01:27:52 +0400

    help: some changes on help string

--- y-lpc-loader.h
+++ y-lpc-loader.h
@@ -1,11 +1,11 @@
 #define HELP_STRING                                             \
   "Usage:\n"                                                    \
-  "-h, --help               this output\n"                       \
-  "-c, --config             file use alternate config file\n"    \
-  "-p, --port               port\n"                              \
-  "-f, --first              first execurable file\n"             \
-  "-s, --second             secondary execurable file\n"         \
-  "-i, --primary-address    primary file load address\n"         \
+  "-h, --help               help\n"                             \
+  "-c, --config             file use alternate config file\n"   \
+  "-p, --port               port\n"                             \
+  "-f, --first              primary execurable file\n"          \
+  "-s, --second             secondary execurable file\n"        \
+  "-i, --primary-address    primary file load address\n"        \
   "-d, --secondary-address  secondary file load address\n"
 int               main (int argc, char *argv[], char *env[]);
 int               setup_port (int port_fd);
